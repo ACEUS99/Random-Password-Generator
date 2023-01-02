89cd4afecb4d9864dcfa39e356ab8306dc021711
Aaman Kudhail <44379373+ACEUS99@users.noreply.github.com>
2023-01-02 06:55:52 +0000

    All files

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordOne = document.getElementById("password-one") 
+let passwordTwo = document.getElementById("password-two") 
+let first = " "
+let second = " "
+
+
+function createPasswords(){
+    first = " "
+    second = " "
+    for (i = 0; i < 15; i++){
+        first += randomlyGenerate()
+        second += randomlyGenerate()
+    }
+ 
+    passwordOne.textContent = first
+    passwordTwo.textContent = second
+}
+
+function randomlyGenerate(){
+    index = Math.floor(Math.random() * 91)
+    return characters[index]
+}
